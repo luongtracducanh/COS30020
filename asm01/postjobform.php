1132084e5f40b4b92470136d959873ba74821eec
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-05-25 19:04:20 +0700

    added new files

--- asm01/postjobform.php
+++ asm01/postjobform.php
@@ -6,13 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JVPS</title>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> -->
 </head>
 
 <body>
     <h1>Job Vacancy Posting System</h1>
-    <form method="POST">
-        <p>Position ID: <input type="text" id="positionId" name="positionId"></p>
+    <form method="POST" action="postjobform.php">
+        <p>Position ID: <input type="text" id="positionId" name="positionId">
+            <?php
+            if (isset($_POST['positionId'])) {
+                if (empty($_POST['positionId'])) {
+                    echo "Please enter a position ID.";
+                } else {
+                    $positionId = $_POST['positionId'];
+                }
+            }
+            ?></p>
         <p>Title: <input type="text" id="title" name="title"></p>
+        <?php
+
+        ?>
         <p>Description: <textarea id="description" name="description"></textarea></p>
         <p>Closing Date: <input type="text" id="closingDate" name="closingDate" value="<?php echo date('d/m/y'); ?>"></p>
         <p>Position:
@@ -22,9 +35,9 @@
             <label for="partTime">Part Time</label>
         </p>
         <p>Contract:
-            <input type="checkbox" id="post" name="applicationMethod[]" value="Post">
+            <input type="checkbox" id="post" name="post" value="Post">
             <label for="post">Post</label>
-            <input type="checkbox" id="email" name="applicationMethod[]" value="Email">
+            <input type="checkbox" id="email" name="email" value="Email">
             <label for="email">Email</label>
         </p>
         <p>Application By: <input type="checkbox"> Post <input type="checkbox">Mail</p>
