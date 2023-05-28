efbbaaf5ea6f5e993ed266594c80cff0f6b946a5
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-05-28 16:31:28 +0700

    added new files

--- asm01/postjobform.php
+++ asm01/postjobform.php
@@ -6,105 +6,34 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>JVPS</title>
-	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> -->
+	<!-- Include a CSS file here if needed -->
 </head>
 
 <body>
 	<h1>Job Vacancy Posting System</h1>
 	<form method="POST" action="postjobprocess.php">
-		<p>Position ID: <input type="text" id="posId" name="posId">
-			<?php
-			if (isset($_POST['posId'])) {
-				if (empty($_POST['posId'])) {
-					echo 'Please enter a position ID.';
-				} else if (!preg_match('/^P\d{4}$/', $_POST['posId'])) {
-					echo 'Please enter a unique 5-character code that starts with "P" and follows by 4 digits e.g. P0001.';
-				} else {
-					$posId = $_POST['posId'];
-				}
-			}
-			?></p>
-		<p>Title: <input type="text" id="title" name="title">
-			<?php
-			if (isset($_POST['title'])) {
-				if (empty($_POST['title'])) {
-					echo 'Please enter a title.';
-				} else if (!preg_match('/^[a-zA-Z0-9 ,.!]{1,20}$/', $_POST['title'])) {
-					echo 'Please enter a maximum of 20 characters without any special characacter (comma, period, and exclamation point are allowed).';
-				} else {
-					$title = $_POST['title'];
-				}
-			}
-			?></p>
-		<p>Description: <textarea id="des" name="des"></textarea>
-			<?php
-			if (isset($_POST['des'])) {
-				if (empty($_POST['des'])) {
-					echo 'Please enter a description.';
-				} else if (strlen($_POST['des']) > 260) {
-					echo 'Please enter a maximum of 260 characters.';
-				} else {
-					$des = $_POST['des'];
-				}
-			}
-			?></p>
-		<p>Closing Date: <input type="text" id="date" name="date" value="<?php echo date('d/m/y'); ?>">
-			<?php
-			if (isset($_POST['date'])) {
-				if (empty($_POST['date'])) {
-					echo 'Please enter a closing date.';
-				} else if (!preg_match('/^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{2}$/', $_POST['date'])) {
-					echo 'Please enter a date in "dd/mm/yy" format.';
-				} else {
-					$date = $_POST['date'];
-				}
-			}
-			?></p>
-		<p>
-			Position:
+		<p>Position ID: <input type="text" id="posId" name="posId"></p>
+		<p>Title: <input type="text" id="title" name="title"></p>
+		<p>Description: <textarea id="des" name="des"></textarea></p>
+		<!-- echo current date using php -->
+		<p>Closing Date: <input type="text" id="date" name="date" value="<?php echo date('d/m/y'); ?>"></p>
+		<p>Position:
 			<input type="radio" id="fullTime" name="position" value="Full Time">
 			<label for="fullTime">Full Time</label>
 			<input type="radio" id="partTime" name="position" value="Part Time">
 			<label for="partTime">Part Time</label>
-			<?php
-			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-				if (!isset($_POST['position'])) {
-					echo '<br>Please select a position.';
-				} else {
-					$position = $_POST['position'];
-				}
-			}
-			?>
 		</p>
 		<p>Contract:
 			<input type="radio" id="onGoing" name="contract" value="On-going">
-			<label for="post">On-going</label>
+			<label for="onGoing">On-going</label>
 			<input type="radio" id="fixedTerm" name="contract" value="Fixed term">
-			<label for="email">Fixed term</label>
-			<?php
-			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-				if (!isset($_POST['contract'])) {
-					echo '<br>Please select a contract.';
-				} else {
-					$contract = $_POST['contract'];
-				}
-			}
-			?>
+			<label for="fixedTerm">Fixed term</label>
 		</p>
 		<p>Accepted application By:
 			<input type="checkbox" id="post" name="app" value="Post">
-			<label for="">Post</label>
+			<label for="post">Post</label>
 			<input type="checkbox" id="mail" name="app" value="Mail">
-			<label for="">Mail</label>
-			<?php
-			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-				if (!isset($_POST['app'])) {
-					echo '<br>Please select a method(s) to send accepted application.';
-				} else {
-					$app = $_POST['app'];
-				}
-			}
-			?>
+			<label for="mail">Mail</label>
 		</p>
 		<p>Location:
 			<select name="location">
@@ -118,19 +47,10 @@
 				<option value="VIC">VIC</option>
 				<option value="WA">WA</option>
 			</select>
-			<?php
-			if (isset($_POST['location'])) {
-				if (empty($_POST['location'])) {
-					echo 'Please enter a location.';
-				} else {
-					$location = $_POST['location'];
-				}
-			}
-			?>
 		</p>
 		<input type="submit" value="Submit">
 		<input type="reset" value="Reset">
-		<p>All fields are required. <a href="index.php">Return to Home Page</a></p>
+		<p>All fields are. <a href="index.php">Return to Home Page</a></p>
 	</form>
 </body>
 
--- asm01/postjobprocess.php
+++ asm01/postjobprocess.php
@@ -1,68 +1,169 @@
-<?php
-if (isset($_POST['posId'])) {
-	if (empty($_POST['posId'])) {
-		echo 'Please enter a position ID.';
-	} else if (!preg_match('/^P\d{4}$/', $_POST['posId'])) {
-		echo 'Please enter a unique 5-character code that starts with "P" and follows by 4 digits e.g. P0001.';
-	} else {
-		$posId = $_POST['posId'];
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>JVPS</title>
+</head>
+
+<body>
+	<h1>Job Vacancy Posting System</h1>
+	<!-- validation section -->
+	<?php
+	// declare variables
+	$posId = $title = $des = $date = $position = $contract = $app = $location = "";
+	$file = 'jobs.txt';
+
+	// Check if the Position ID is unique
+	function isPosIdUnique($posId, $file)
+	{
+		$handle = fopen($file, 'r');
+		if ($handle) {
+			while (($line = fgets($handle)) !== false) {
+				$lineData = explode("\t", $line);
+				if (isset($lineData[0]) && trim($lineData[0]) === $posId) {
+					fclose($handle);
+					return false; // Position ID already exists
+				}
+			}
+			fclose($handle);
+		}
+		return true; // Position ID is unique
 	}
-}
-if (isset($_POST['title'])) {
-	if (empty($_POST['title'])) {
-		echo 'Please enter a title.';
-	} else if (!preg_match('/^[a-zA-Z0-9 ,.!]{1,20}$/', $_POST['title'])) {
-		echo 'Please enter a maximum of 20 characters without any special characacter (comma, period, and exclamation point are allowed).';
-	} else {
-		$title = $_POST['title'];
+
+
+	// validate position id field
+	if (isset($_POST['posId'])) {
+		// check if the field is empty
+		if (empty($_POST['posId'])) {
+			echo '<p>Please enter a position ID.</p>';
+		}
+		// check for uniqueness
+		else if (!isPosIdUnique($_POST['posId'], $file)) {
+			echo '<p>The position ID already exists. Please enter a unique ID.</p>';
+		}
+		// check if the input matches the pattern
+		else if (!preg_match('/^P\d{4}$/', $_POST['posId'])) {
+			echo '<p>Please enter a unique 5-character code that starts with "P" and follows by 4 digits e.g. P0001.</p>';
+		}
+		// if validated, assign the value to a variable
+		else {
+			$posId = $_POST['posId'];
+		}
 	}
-}
-if (isset($_POST['des'])) {
-	if (empty($_POST['des'])) {
-		echo 'Please enter a description.';
-	} else if (strlen($_POST['des']) > 260) {
-		echo 'Please enter a maximum of 260 characters.';
-	} else {
-		$des = $_POST['des'];
+
+	// validate the title field
+	if (isset($_POST['title'])) {
+		// check if the field is empty
+		if (empty($_POST['title'])) {
+			echo '<p>Please enter a title.</p>';
+		}
+		// check if the input matches the pattern
+		else if (!preg_match('/^[a-zA-Z0-9 ,.!]{1,20}$/', $_POST['title'])) {
+			echo '<p>Please enter a maximum of 20 characters without any special characacter (comma, period, and exclamation point are allowed).</p>';
+		}
+		// if validated, assign the value to a variable
+		else {
+			$title = $_POST['title'];
+		}
 	}
-}
-if (isset($_POST['date'])) {
-	if (empty($_POST['date'])) {
-		echo 'Please enter a closing date.';
-	} else if (!preg_match('/^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{2}$/', $_POST['date'])) {
-		echo 'Please enter a date in "dd/mm/yy" format.';
-	} else {
-		$date = $_POST['date'];
+
+	// validate the description field
+	if (isset($_POST['des'])) {
+		// check if the field is empty
+		if (empty($_POST['des'])) {
+			echo '<p>Please enter a description.</p>';
+		}
+		// check if the input exceeds 260 characters
+		else if (strlen($_POST['des']) > 260) {
+			echo '<p>Please enter a maximum of 260 characters.</p>';
+		}
+		// if validated, assign the value to a variable
+		else {
+			$des = $_POST['des'];
+		}
 	}
-}
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+	// validate the closing date field
+	if (isset($_POST['date'])) {
+		// check if the field is empty
+		if (empty($_POST['date'])) {
+			echo '<p>Please enter a closing date.</p>';
+		}
+		// check if the input matches dd/mm/yy
+		else if (!preg_match('/^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{2}$/', $_POST['date'])) {
+			echo '<p>Please enter a date in "dd/mm/yy" format.</p>';
+		}
+		// if validated, assign the value to a variable
+		else {
+			$date = $_POST['date'];
+		}
+	}
+
+	// check if a position a selected
 	if (!isset($_POST['position'])) {
-		echo '<br>Please select a position.';
-	} else {
+		echo '<p>Please select a position.</p>';
+	}
+	// if validated, assign the value to a variable
+	else {
 		$position = $_POST['position'];
 	}
-}
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// check if a contract type is selected
 	if (!isset($_POST['contract'])) {
-		echo '<br>Please select a contract.';
-	} else {
+		echo '<p>Please select a contract.</p>';
+	}
+	// if validated, assign the value to a variable
+	else {
 		$contract = $_POST['contract'];
 	}
-}
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// check if an application method is selected
 	if (!isset($_POST['app'])) {
-		echo '<br>Please select a method(s) to send accepted application.';
-	} else {
+		echo '<p>Please select a method(s) to send accepted application.</p>';
+	}
+	// if validated, assign the value to a variable
+	else {
 		$app = $_POST['app'];
 	}
-}
 
-if (isset($_POST['location'])) {
-	if (empty($_POST['location'])) {
-		echo 'Please enter a location.';
-	} else {
-		$location = $_POST['location'];
+	// validate the location option menu
+	if (isset($_POST['location'])) {
+		// check if a location is selected
+		if (empty($_POST['location'])) {
+			echo '<p>Please enter a location.</p>';
+		}
+		// if validated, assign the value to a variable
+		else {
+			$location = $_POST['location'];
+		}
 	}
-}
+	?>
+
+	<!-- write to text file -->
+	<?php
+	if (!empty($posId) && !empty($title) && !empty($des) && !empty($date) && !empty($position) && !empty($contract) && !empty($app) && !empty($location)) {
+		// All variables are not empty strings
+		$record = "$posId\t$title\t$des\t$date\t$position\t$contract\t$app\t$location\n";
+		// Open the file in append mode
+		$handle = fopen($file, 'a');
+
+		if ($handle) {
+			// Append the new line to the file
+			fwrite($handle, $record);
+
+			// Close the file handle
+			fclose($handle);
+
+			echo $record;
+		} else {
+			echo "Unable to open the file.";
+		}
+	}
+	?>
+
+</body>
+
+</html>
