b6e460e725302490bffc7338da14d874306cbb6c
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-06-09 07:57:05 +0700

    added new files

--- a.php
+++ a.php
@@ -4,8 +4,11 @@ function mySort($a, $b)
   return $a <=> $b;
 }
 
-$numbers = array(12, 6463, 1, 5235, 5);
-usort($numbers, "mySort");
+$numbers = array(1, 4, 3, 2, 5);
+usort($numbers, function ($a, $b)
+{
+  return $a <=> $b;
+});
 
 for ($i = 0; $i < count($numbers); $i++) {
   echo $numbers[$i] . ", ";
