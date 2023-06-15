3617bfa5145408f11cec91235cb5b1c8e0c34402
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-06-15 15:04:34 +0700

    added new files

--- asm01/postjobform.php
+++ asm01/postjobform.php
@@ -10,103 +10,103 @@
 </head>
 
 <body>
-<h1 class="header">Job Vacancy Posting System</h1>
-    <nav>
+  <h1 class="header">Job Vacancy Posting System</h1>
+  <nav>
     <ul class="navigator">
       <li class="navlist"> <a href="index.php">Home</a></li>
       <li class="navlist current"> <a href="postjobform.php">Post job</a></li>
       <li class="navlist"> <a href="searchjobform.php"> Search job</a></li>
       <li class="navlist"> <a href="about.php">About assignment</a></li>
-  </ul>
+    </ul>
   </nav>
   <div class="container">
-  <form method="POST" action="postjobprocess.php">
-    <div class="row">
-      <div class="col-25">
-        <p>Position ID:</p>
+    <form method="POST" action="postjobprocess.php">
+      <div class="row">
+        <div class="col-25">
+          <p>Position ID:</p>
+        </div>
+        <div class="col-75">
+          <p><input type="text" id="posId" name="posId"></p>
+        </div>
       </div>
-      <div class="col-75">    
-        <p><input type="text" id="posId" name="posId"></p>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-25">
-        <p>Title:</p>
+      <div class="row">
+        <div class="col-25">
+          <p>Title:</p>
+        </div>
+        <div class="col-75">
+          <p> <input type="text" id="title" name="title"></p>
+        </div>
       </div>
-      <div class="col-75">    
-      <p> <input type="text" id="title" name="title"></p>
-      </div>
-    </div> 
 
-    <div class="row">
-      <div class="col-25">
-        <p>Description:</p>
+      <div class="row">
+        <div class="col-25">
+          <p>Description:</p>
+        </div>
+        <div class="col-75">
+          <p> <textarea id="des" name="des"></textarea></p>
+          <p class="note"><i>Note: The description criteria will be displayed in a bullet point list.<br>Simply enter each criteria on a new line in the text area, it will be automatically created the list.</i></p>
+        </div>
       </div>
-      <div class="col-75">    
-      <p> <textarea id="des" name="des"></textarea></p>
-      <p class="note"><i>Note: The description criteria will be displayed in a bullet point list.<br>Simply enter each criteria on a new line in the text area, it will be automatically created the list.</i></p>
-      </div>
-    </div> 
 
-    <div class="row">
-      <div class="col-25">
-        <!-- echo current date using php -->
-        <p>Closing Date:</p>
+      <div class="row">
+        <div class="col-25">
+          <!-- echo current date using php -->
+          <p>Closing Date:</p>
+        </div>
+        <div class="col-75">
+          <p> <input type="text" id="date" name="date" placeholder="dd/mm/yy" value="<?php echo date('d/m/y'); ?>"></p>
+        </div>
       </div>
-      <div class="col-75">    
-      <p> <input type="text" id="date" name="date" placeholder="dd/mm/yy" value="<?php echo date('d/m/y'); ?>"></p>
-      </div>
-    </div> 
 
-    <div class="row">
-      <div class="col-25">
-        <p>Position:</p>
+      <div class="row">
+        <div class="col-25">
+          <p>Position:</p>
+        </div>
+        <div class="col-75">
+          <p>
+            <input type="radio" id="fullTime" name="position" value="Full Time">
+            <label for="fullTime">Full Time</label>
+            <input type="radio" id="partTime" name="position" value="Part Time">
+            <label for="partTime">Part Time</label>
+          </p>
+        </div>
       </div>
-      <div class="col-75">  
-        <p> 
-          <input type="radio" id="fullTime" name="position" value="Full Time">
-          <label for="fullTime">Full Time</label>
-          <input type="radio" id="partTime" name="position" value="Part Time">
-          <label for="partTime">Part Time</label>
-        </p>
-      </div>
-    </div> 
 
-    <div class="row">
-      <div class="col-25">
-        <p>Contract:</p>
+      <div class="row">
+        <div class="col-25">
+          <p>Contract:</p>
+        </div>
+        <div class="col-75">
+          <p>
+            <input type="radio" id="onGoing" name="contract" value="On-going">
+            <label for="onGoing">On-going</label>
+            <input type="radio" id="fixedTerm" name="contract" value="Fixed term">
+            <label for="fixedTerm">Fixed term</label>
+          </p>
+        </div>
       </div>
-      <div class="col-75">   
-        <p>
-          <input type="radio" id="onGoing" name="contract" value="On-going">
-          <label for="onGoing">On-going</label>
-          <input type="radio" id="fixedTerm" name="contract" value="Fixed term">
-          <label for="fixedTerm">Fixed term</label>
-        </p>
-      </div>
-    </div> 
 
-    <div class="row">
-      <div class="col-25">
-        <p>Accepted application by:</p>
+      <div class="row">
+        <div class="col-25">
+          <p>Accepted application by:</p>
+        </div>
+        <div class="col-75">
+          <p>
+            <input type="checkbox" id="post" name="app[]" value="Post">
+            <label for="post">Post</label>
+            <input type="checkbox" id="mail" name="app[]" value="Mail">
+            <label for="mail">Mail</label>
+          </p>
+        </div>
       </div>
-      <div class="col-75">  
-        <p> 
-          <input type="checkbox" id="post" name="app[]" value="Post">
-          <label for="post">Post</label>
-          <input type="checkbox" id="mail" name="app[]" value="Mail">
-          <label for="mail">Mail</label>
-        </p>
-      </div>
-    </div> 
 
-    <div class="row">
-      <div class="col-25">
-        <p>Location:</p>
-      </div>
-      <div class="col-75">   
-        
+      <div class="row">
+        <div class="col-25">
+          <p>Location:</p>
+        </div>
+        <div class="col-75">
+
           <select name="location">
             <option value="">---</option>
             <option value="ACT">ACT</option>
@@ -118,23 +118,24 @@
             <option value="VIC">VIC</option>
             <option value="WA">WA</option>
           </select>
-        
-      </div>
-    </div> 
 
-    <div class="submit">
-      <input class="button" type="submit" value="Submit">
-      <input class="button" type="reset" value="Reset">
-    </div>
-    <div class="lastnote">
-      <p class="note">All fields are required.</p>
-      <p class="return"><a href="index.php"><span>Return to Home Page</span></a></p>
-    </div>
-  </form>
-  <div class="push"></div>
+        </div>
+      </div>
+
+      <div class="submit">
+        <input class="button" type="submit" value="Submit">
+        <input class="button" type="reset" value="Reset">
+      </div>
+      <div class="lastnote">
+        <p class="note">All fields are required.</p>
+        <p class="return"><a href="index.php"><span>Return to Home Page</span></a></p>
+      </div>
+    </form>
+    <div class="push"></div>
   </div>
   <footer class="footer">
     <p>Copyright @2023</p>
   </footer>
 </body>
+
 </html>
--- asm01/searchjobprocess.php
+++ asm01/searchjobprocess.php
@@ -10,146 +10,147 @@
 </head>
 
 <body>
-<h1 class="header">Job Vacancy Posting System</h1>
-    <nav>
+  <h1 class="header">Job Vacancy Posting System</h1>
+  <nav>
     <ul class="navigator">
       <li class="navlist"> <a href="index.php">Home</a></li>
       <li class="navlist"> <a href="postjobform.php">Post job</a></li>
       <li class="navlist current"> <a href="searchjobform.php"> Search job</a></li>
       <li class="navlist"> <a href="about.php">About assignment</a></li>
-  </ul>
+    </ul>
   </nav>
   <div class="container">
     <div>
-  <?php
-  $currentDate = new DateTime(); // Get the current date
+      <?php
+      $currentDate = new DateTime(); // Get the current date
 
-  // function for special feature - replace Vietnamese characters with English characters for search filtering
-  function replaceVnese($str)
-  {
-    // lowercase
-    $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
-    $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
-    $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
-    $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
-    $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
-    $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
-    $str = preg_replace("/(đ)/", 'd', $str);
-    // uppercase (not needed since the job titles will be converted to lowercase before being passed to this function)
-    // $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
-    // $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
-    // $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
-    // $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
-    // $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
-    // $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
-    // $str = preg_replace("/(Đ)/", 'D', $str);
-    return $str;
-  }
-
-  // check if the job title is set and not empty
-  if (isset($_GET['title']) && !empty($_GET['title'])) {
-    $title = $_GET['title'];
-    $position = isset($_GET["position"]) ? $_GET["position"] : "";
-    $contract = isset($_GET["contract"]) ? $_GET["contract"] : "";
-    $app = isset($_GET["app"]) ? $_GET["app"] : array();
-    $location = isset($_GET["location"]) ? $_GET["location"] : "";
-
-    $file = '../../data/jobposts/jobs.txt';
-    if (!file_exists($file)) {
-      echo "<p class='process'>No up-to-date job vacancy found.</p>";
-      exit;
-    } else {
-      $handle = fopen($file, 'r');
-      // check if the file is opened successfully
-      if ($handle) {
-        $jobVacancies = array(); // array to store the job vacancies
-        while (($line = fgets($handle)) !== false) {
-          $lineData = explode("\t", $line);
-          $jobTitle = isset($lineData[1]) ? $lineData[1] : "";
-          $des = isset($lineData[2]) ? stripslashes($lineData[2]) : "";
-          $closingDate = isset($lineData[3]) ? date_create_from_format('d/m/y', $lineData[3]) : "";
-          $jobPosition = isset($lineData[4]) ? $lineData[4] : "";
-          $jobContract = isset($lineData[5]) ? $lineData[5] : "";
-          $jobApplication = isset($lineData[6]) ? explode(", ", $lineData[6]) : array();
-          $jobLocation = isset($lineData[7]) ? $lineData[7] : "";
-          // check if the job vacancy matches the search criteria
-          if (
-            (strpos(replaceVnese(strtolower($jobTitle)), replaceVnese(strtolower(trim($title)))) !== false) &&
-            // (strpos(strtolower(trim($jobTitle)), strtolower($title)) !== false) &&
-            (empty($position) || strtolower(trim($jobPosition)) === strtolower($position)) &&
-            (empty($contract) || strtolower(trim($jobContract)) === strtolower($contract)) &&
-            (empty($app) || array_intersect(array_map('strtolower', $app), array_map('strtolower', $jobApplication))) &&
-            (empty($location) || strtolower(trim($jobLocation)) === strtolower($location)) &&
-            ($closingDate >= $currentDate) // check if the closing date is after or equal to the current date
-          ) {
-            // add the job vacancy to the array
-            $jobVacancies[] = array(
-              'title' => $jobTitle,
-              'description' => $des,
-              'closingDate' => $closingDate,
-              'position' => "$jobContract - $jobPosition",
-              'application' => $jobApplication,
-              'location' => $jobLocation
-            );
-          }
-        }
-        if (empty($jobVacancies)) {
-          echo "<p class='process'>No up-to-date job vacancy found.</p>";
-        } else {
-          // sort the job vacancies by closing date in descending order
-          usort($jobVacancies, function ($a, $b) {
-            if ($a['closingDate'] < $b['closingDate']) {
-              return -1;
-            } elseif ($a['closingDate'] > $b['closingDate']) {
-              return 1;
-            } else {
-              return 0;
-            }
-          });
-
-          // iterate over the sorted job vacancies and display the information for the ones that haven't closed
-          foreach ($jobVacancies as $job) {
-            $jobTitle = $job['title'];
-            $description = $job['description'];
-            $descriptionList = explode("[NEWLINE]", $description);
-            $closingDate = $job['closingDate'];
-            $position = $job['position'];
-            $application = implode(", ", $job['application']);
-            $location = $job['location'];
-
-            // display the job vacancy information
-          echo '<div class="jobvacancy">';
-            echo "<p class='jobtitle'> Job Title: $jobTitle</p>";
-            echo "<span class='list'>Description:</span>";
-            echo "<ul>";
-            foreach ($descriptionList as $item) {
-              echo "<li>" . htmlspecialchars($item) . "</li>";
-            }
-            echo "</ul>";
-            echo "<p><span class='list'>Closing Date:</span> {$closingDate->format('d/m/y')}</p>";
-            echo "<p><span class='list'>Position:</span> $position</p>";
-            echo "<p><span class='list'>Application by:</span> $application</p>";
-            echo "<p><span class='list'>Location:</span> $location</p>";
-          echo '</div>';
-
-          }
-        }
-        fclose($handle); // close the file
-      } else {
-        echo "Unable to open $file.";
+      // function for special feature - replace Vietnamese characters with English characters for search filtering
+      function replaceVnese($str)
+      {
+        // lowercase
+        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+        $str = preg_replace("/(đ)/", 'd', $str);
+        // uppercase (not needed since the job titles will be converted to lowercase before being passed to this function)
+        // $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+        // $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+        // $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+        // $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+        // $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+        // $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+        // $str = preg_replace("/(Đ)/", 'D', $str);
+        return $str;
       }
-    }
-  } else {
-    echo '<p class="process">Please enter a job title.</p>';
-  }
-  ?>
-  </div>
-  
-  <div class="lastnote">
-    <p class="return"><a href="searchjobform.php"><span>Search for another job vacancy</span></a></p>
-    <p class="return"><a href="index.php"><span>Return to Home Page</span></a></p>
-  </div>
-  <div class="push"></div>
+
+      // check if the job title is set and not empty
+      if (isset($_GET['title']) && !empty($_GET['title'])) {
+        $title = $_GET['title'];
+        $position = isset($_GET["position"]) ? $_GET["position"] : "";
+        $contract = isset($_GET["contract"]) ? $_GET["contract"] : "";
+        $app = isset($_GET["app"]) ? $_GET["app"] : array();
+        $location = isset($_GET["location"]) ? $_GET["location"] : "";
+
+        $file = '../../data/jobposts/jobs.txt';
+        if (!file_exists($file)) {
+          echo "<p class='process'>No up-to-date job vacancy found.</p>";
+          exit;
+        } else {
+          $handle = fopen($file, 'r');
+          // check if the file is opened successfully
+          if ($handle) {
+            $jobVacancies = array(); // array to store the job vacancies
+            while (($line = fgets($handle)) !== false) {
+              $lineData = explode("\t", $line);
+              $jobTitle = isset($lineData[1]) ? $lineData[1] : "";
+              $des = isset($lineData[2]) ? stripslashes($lineData[2]) : "";
+              $closingDate = isset($lineData[3]) ? date_create_from_format('d/m/y', $lineData[3]) : "";
+              $jobPosition = isset($lineData[4]) ? $lineData[4] : "";
+              $jobContract = isset($lineData[5]) ? $lineData[5] : "";
+              $jobApplication = isset($lineData[6]) ? explode(", ", $lineData[6]) : array();
+              $jobLocation = isset($lineData[7]) ? $lineData[7] : "";
+              // check if the job vacancy matches the search criteria
+              if (
+                (strpos(replaceVnese(strtolower($jobTitle)), replaceVnese(strtolower(trim($title)))) !== false) &&
+                // (strpos(strtolower(trim($jobTitle)), strtolower($title)) !== false) &&
+                (empty($position) || strtolower(trim($jobPosition)) === strtolower($position)) &&
+                (empty($contract) || strtolower(trim($jobContract)) === strtolower($contract)) &&
+                (empty($app) || array_intersect(array_map('strtolower', $app), array_map('strtolower', $jobApplication))) &&
+                (empty($location) || strtolower(trim($jobLocation)) === strtolower($location)) &&
+                ($closingDate >= $currentDate) // check if the closing date is after or equal to the current date
+              ) {
+                // add the job vacancy to the array
+                $jobVacancies[] = array(
+                  'title' => $jobTitle,
+                  'description' => $des,
+                  'closingDate' => $closingDate,
+                  'position' => "$jobContract - $jobPosition",
+                  'application' => $jobApplication,
+                  'location' => $jobLocation
+                );
+              }
+            }
+            if (empty($jobVacancies)) {
+              echo "<p class='process'>No up-to-date job vacancy found.</p>";
+            } else {
+              // sort the job vacancies by closing date in descending order
+              usort($jobVacancies, function ($a, $b) {
+                if ($a['closingDate'] < $b['closingDate']) {
+                  return -1;
+                } elseif ($a['closingDate'] > $b['closingDate']) {
+                  return 1;
+                } else {
+                  return 0;
+                }
+              });
+
+              // iterate over the sorted job vacancies and display the information for the ones that haven't closed
+              foreach ($jobVacancies as $job) {
+                $jobTitle = $job['title'];
+                $description = $job['description'];
+                // $descriptionList = explode("[NEWLINE]", $description);
+                $descriptionList = str_replace("[NEWLINE]", "<br>", $description);
+                $closingDate = $job['closingDate'];
+                $position = $job['position'];
+                $application = implode(", ", $job['application']);
+                $location = $job['location'];
+
+                // display the job vacancy information
+                echo '<div class="jobvacancy">';
+                echo "<p class='jobtitle'> Job Title: $jobTitle</p>";
+                echo "<span class='list'>Description:</span>";
+                echo "<p>$descriptionList</p>";
+                // echo "<ul>";
+                // foreach ($descriptionList as $item) {
+                //   echo "<li>" . htmlspecialchars($item) . "</li>";
+                // }
+                // echo "</ul>";
+                echo "<p><span class='list'>Closing Date:</span> {$closingDate->format('d/m/y')}</p>";
+                echo "<p><span class='list'>Position:</span> $position</p>";
+                echo "<p><span class='list'>Application by:</span> $application</p>";
+                echo "<p><span class='list'>Location:</span> $location</p>";
+                echo '</div>';
+              }
+            }
+            fclose($handle); // close the file
+          } else {
+            echo "Unable to open $file.";
+          }
+        }
+      } else {
+        echo '<p class="process">Please enter a job title.</p>';
+      }
+      ?>
+    </div>
+
+    <div class="lastnote">
+      <p class="return"><a href="searchjobform.php"><span>Search for another job vacancy</span></a></p>
+      <p class="return"><a href="index.php"><span>Return to Home Page</span></a></p>
+    </div>
+    <div class="push"></div>
   </div>
   <footer class="footer">
     <p>Copyright @2023</p>
